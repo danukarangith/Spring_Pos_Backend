559991e6d495fa8aedc7d164c3783b9d3538016a
danukarangith <danukarangith456@gmail.com>
2024-10-14 23:45:02 +0530

    web app config

--- src/main/java/lk/ijse/spring_pos_backend/config/WebAppConfig.java
+++ src/main/java/lk/ijse/spring_pos_backend/config/WebAppConfig.java
@@ -1,4 +1,14 @@
 package lk.ijse.spring_pos_backend.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-public class WebAppConfig {
-}
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring_pos_backend")
+@EnableWebMvc
+@EnableJpaRepositories(basePackages = "lk.ijse.spring_pos_backend")
+@EnableTransactionManagement
+
+public class WebAppConfig {}
